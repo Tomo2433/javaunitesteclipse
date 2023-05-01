fb5e50e54fb8d5ed0055348556727bfda1d0e9aa
Tomo2433 <tomaszlech56@gmail.com>
2023-05-01 22:17:11 +0200

    Menu bar added to main window

--- src/GuiApp/MyWindow.java
+++ src/GuiApp/MyWindow.java
@@ -2,16 +2,20 @@ package GuiApp;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
+import java.net.URL;
 
 public class MyWindow extends JFrame implements ActionListener {
 
+    private static final long serialVersionUID = 1L;
     private final int WINDOW_WIDTH_SIZE = 650;
     private final int WINDOW_HEIGHT_SIZE = 450;
+    private static final String ICON_PATH = "/resources/";
 
+    //icons definition
+    private Icon iconExit, mIconExit, iconAbout, mIconAbout, iconHelp, mIconHelp;
+
+    //menu variables definition
     private JMenu fileMenu, editMenu, viewMenu, calculationsMenu, helpMenu;
     private JMenuItem exitMenuItem, aboutMenuItem, helpMenuItem;
 
@@ -34,7 +38,9 @@ public class MyWindow extends JFrame implements ActionListener {
             javax.swing.SwingUtilities.invokeAndWait(new Runnable() {
                 @Override
                 public void run() {
-
+                    createIcons();
+                    createMenus();
+                    contentPane.add(createCenterPanel(), BorderLayout.CENTER);
                 }
             });
         }
@@ -42,7 +48,51 @@ public class MyWindow extends JFrame implements ActionListener {
             System.out.println("ERROR - error while creating gui of application");
         }
     }
+    private void createIcons(){
+        //create 24x24px icons for toolbar
+        iconExit = createMyIcon("close.jpg");
+        iconAbout = createMyIcon("about.jpg");
+        iconHelp = createMyIcon("help_context.jpg");
+        //create 16x16px icons for menubar
+        mIconExit = createMyIcon("min_close.jpg");
+        mIconAbout = createMyIcon("min_about.jpg");
+        mIconHelp = createMyIcon("min_help_context.jpg");
+    }
+    private void createMenus(){
+        //create menu bar
+        JMenuBar menuBar = new JMenuBar();
+        //create main menu poles
+        fileMenu = createJMenu("Plik", KeyEvent.VK_P);
+        editMenu = createJMenu("Edycja", KeyEvent.VK_E);
+        viewMenu = createJMenu("Widok", KeyEvent.VK_W);
+        calculationsMenu = createJMenu("Obliczenia", KeyEvent.VK_O);
+        helpMenu = createJMenu("Pomoc", KeyEvent.VK_I);
+        //create menu item
+        exitMenuItem = createJMenuItem("Zamknij",mIconExit,
+                KeyStroke.getKeyStroke(KeyEvent.VK_X, ActionEvent.ALT_MASK));
+        aboutMenuItem = createJMenuItem("O autorze",mIconAbout,
+                KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.ALT_MASK));
+        helpMenuItem = createJMenuItem("Kontekst pomocy",mIconHelp,
+                KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.ALT_MASK));
 
+        fileMenu.add(exitMenuItem);
+        helpMenu.add(helpMenuItem);
+        helpMenu.add(aboutMenuItem);
+
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+        menuBar.add(viewMenu);
+        menuBar.add(calculationsMenu);
+        menuBar.add(helpMenu);
+        setJMenuBar(menuBar);
+    }
+    public Icon createMyIcon(String nameFile){
+        String name = ICON_PATH + nameFile;
+        Icon icon = null;
+        URL url = getClass().getResource(name);
+        if(url != null) icon = new ImageIcon(url);
+        return icon;
+    }
     public JMenu createJMenu(String name, int keyEvent){
         JMenu jMenu = new JMenu(name);
         jMenu.setMnemonic(keyEvent);
@@ -62,11 +112,11 @@ public class MyWindow extends JFrame implements ActionListener {
 
         return jToolBar;
     }
-    private JButton createJButtonToolBar(String tooltip, Icon icon){
+    private JButton createJButtonToolBar(String tooltip, Icon icon) {
         JButton jButton = new JButton("", icon);
         jButton.setToolTipText(tooltip);
         jButton.addActionListener(this);
-        return  jButton;
+        return jButton;
     }
     private JPanel createCenterPanel(){
         JPanel jPanel = new JPanel();
