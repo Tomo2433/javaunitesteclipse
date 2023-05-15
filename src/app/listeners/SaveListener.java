88dd9e4e16c147872f57f76abbfa14e5b0645bf7
Tomo2433 <tomaszlech56@gmail.com>
2023-05-15 19:13:17 +0200

    new save method

--- src/app/listeners/SaveListener.java
+++ src/app/listeners/SaveListener.java
@@ -1,10 +1,12 @@
 package app.listeners;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 public class SaveListener implements ActionListener {
@@ -20,24 +22,34 @@ public class SaveListener implements ActionListener {
         this.textArea = textArea;
     }
     public void actionPerformed(ActionEvent e){
-        file = new File("tabela.txt");
-        try {
-            printWriter = new PrintWriter(file);
-            for(int i=1; i<=rows; i++){
-                for (int j=1; j<=cols; j++){
-                    printWriter.print("["+i+","+j+"]");
-                    printWriter.print(": ");
-                    printWriter.print(table.getValueAt(i-1,j-1));
-                    printWriter.print("\t");
-                }
-                printWriter.println("");
+        JFileChooser jFileChooser = new JFileChooser();
+        jFileChooser.setDialogTitle("Zapisz plik");
+        jFileChooser.setFileFilter(new FileNameExtensionFilter("Text files (*.txt)", "txt"));
+        JFrame fileFrame = new JFrame();
+        if(jFileChooser.showSaveDialog(fileFrame) == JFileChooser.APPROVE_OPTION){
+            File file = jFileChooser.getSelectedFile();
+            if (!file.getName().endsWith(".txt")) {
+                file = new File(file.getAbsolutePath() + ".txt");
             }
-            printWriter.close();
-            textArea.append("Zapisano wartości tabeli do pliku tabela.txt\n");
-        }catch (FileNotFoundException fnfe){
-            JFrame frame = new JFrame();
-            JOptionPane.showMessageDialog(frame,
-                    "Błąd nie znaleziono pliku!!!");
+            try {
+                printWriter = new PrintWriter(file);
+                for(int i=1; i<=rows; i++){
+                    for (int j=1; j<=cols; j++){
+                        printWriter.print("["+i+","+j+"]");
+                        printWriter.print(": ");
+                        printWriter.print(table.getValueAt(i-1,j-1));
+                        printWriter.print("\t");
+                    }
+                    printWriter.println("");
+                }
+                printWriter.close();
+                textArea.append("Zapisano wartości tabeli do pliku "+file.getName()+"\n");
+            }catch (IOException ioex){
+                JFrame frame = new JFrame();
+                JOptionPane.showMessageDialog(frame,
+                        "Błąd nie znaleziono pliku!!!");
+                ioex.printStackTrace();
+            }
         }
     }
 }
